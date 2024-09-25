e86a65dbf076fec583873fbe6fa932eac7f3d297
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2024-09-25 17:23:10 +0500

    changes added

--- app/src/main/java/com/example/wallpaperapp/fragments/SplashFragment.kt
+++ app/src/main/java/com/example/wallpaperapp/fragments/SplashFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.example.wallpaperapp.R
 import com.example.wallpaperapp.databinding.FragmentSplashBinding
 import kotlinx.coroutines.delay
@@ -28,12 +29,10 @@ class SplashFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         val fadeIn = AnimationUtils.loadAnimation(requireContext(), R.anim.fade_in)
         val slideUp = AnimationUtils.loadAnimation(requireContext(), R.anim.slide_up)
 
         binding.apply {
-
             txtSplashWelcome.visibility = View.GONE
             splashLayout.visibility = View.GONE
             buttonStart.visibility = View.GONE
@@ -42,13 +41,20 @@ class SplashFragment : Fragment() {
         lifecycleScope.launch {
             delay(2000)
 
-            binding.txtSplashWelcome.visibility = View.VISIBLE
-            binding.splashLayout.visibility = View.VISIBLE
-            binding.buttonStart.visibility = View.VISIBLE
-            binding.buttonStart.startAnimation(slideUp)
-            binding.txtSplashWelcome.startAnimation(slideUp)
-            binding.splashLayout.startAnimation(fadeIn)
+            binding.apply {
+                txtSplashWelcome.visibility = View.VISIBLE
+                splashLayout.visibility = View.VISIBLE
+                buttonStart.visibility = View.VISIBLE
+                buttonStart.startAnimation(slideUp)
+                txtSplashWelcome.startAnimation(slideUp)
+                splashLayout.startAnimation(fadeIn)
+            }
+        }
+
+        binding.buttonStart.setOnClickListener {
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
         }
 
     }
+
 }
---
+++ app/src/main/java/com/example/wallpaperapp/fragments/home/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.example.wallpaperapp.fragments.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.wallpaperapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.toolbarComponent.textToolbar.text="Wallify"
+    }
+
+
+
+}
