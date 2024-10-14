a8da5638fdd2e36e64162cb94dd155bfdbde31db
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2024-10-14 14:22:38 +0500

    bottom nav bar issue solved

--- app/src/main/java/com/example/wallpaperapp/MainActivity.kt
+++ app/src/main/java/com/example/wallpaperapp/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.wallpaperapp
 
 import android.os.Bundle
 import android.view.View
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.wallpaperapp.databinding.ActivityMainBinding
@@ -19,28 +16,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        enableEdgeToEdge()
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        val navController = navHostFragment.navController
+//        val navHostFragment =
+//            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+//        val navController = navHostFragment.navController
+        val navController =
+            binding.fragmentContainerView.getFragment<NavHostFragment>().navController
 
-        binding.bottomNavView.setupWithNavController(navController)
+
+        binding.bottomNavView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> {
+                    navController.navigate(R.id.homeFragment)
+                    true
+                }
+                R.id.searchFragment -> {
+                    navController.navigate(R.id.searchFragment)
+                    true
+                }
+                R.id.favouriteFragment -> {
+                    navController.navigate(R.id.favouriteFragment)
+                    true
+                }
+                else -> false
+            }
+        }
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
                 R.id.splashFragment -> hideBottomNav()
                 R.id.homeFragment -> showBottomNav()
-                R.id.searchFragment->hideBottomNav()
-                R.id.fullScreenImageFragment->hideBottomNav()
+                R.id.searchFragment -> hideBottomNav()
+                R.id.wallpaperFragment -> hideBottomNav()
+                R.id.fullScreenImageFragment -> hideBottomNav()
                 else -> showBottomNav()
             }
         }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 
     private fun showBottomNav() {
