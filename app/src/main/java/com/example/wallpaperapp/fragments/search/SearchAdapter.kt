efae002080275508fa39fe224ba4b6b0b4fc35e0
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2024-10-14 14:25:01 +0500

    pagination added

--- app/src/main/java/com/example/wallpaperapp/fragments/search/SearchAdapter.kt
+++ app/src/main/java/com/example/wallpaperapp/fragments/search/SearchAdapter.kt
@@ -1,46 +1,52 @@
 package com.example.wallpaperapp.fragments.search
 
-import com.example.wallpaperapp.model.Photo
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.wallpaperapp.R
 import com.example.wallpaperapp.databinding.WallpaperCardItemBinding
 import com.example.wallpaperapp.fragments.wallpaper.onImageClick
+import com.example.wallpaperapp.model.Photo
 
-class SearchAdapter(private val listener: onImageClick) : RecyclerView.Adapter<SearchAdapter.MyViewHolder>() {
+class SearchAdapter(private val listener: onImageClick) :
+    PagingDataAdapter<Photo, SearchAdapter.MyViewHolder>(WallpaperDiffCallback()) {
 
-    private var wallpapers: List<Photo> = emptyList()
-
-    inner class MyViewHolder( val binding: WallpaperCardItemBinding) :
+    inner class MyViewHolder(val binding: WallpaperCardItemBinding) :
         RecyclerView.ViewHolder(binding.root)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchAdapter.MyViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val binding =
             WallpaperCardItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return MyViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: SearchAdapter.MyViewHolder, position: Int) {
-        val wallpaper = wallpapers[position]
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        // Use getItem() to get the item at the current position
+        val wallpaper = getItem(position)
 
-        Glide.with(holder.itemView.context)
-            .load(wallpaper.src.portrait)
-            .placeholder(R.drawable.place_holderimage)
-            .into(holder.binding.imageViewCategory)
+        // Check if the wallpaper is null (Paging can send null placeholders)
+        wallpaper?.let {
+            Glide.with(holder.itemView.context)
+                .load(wallpaper.src.portrait)
+                .placeholder(R.drawable.place_holderimage)
+                .into(holder.binding.imageViewCategory)
 
-        holder.binding.imageViewCategory.setOnClickListener {
-            listener.onPhotoClick(wallpaper.src.original)
+            holder.binding.imageViewCategory.setOnClickListener {
+                listener.onPhotoClick(wallpaper.src.original)
+            }
+        }
+    }
+
+    class WallpaperDiffCallback : DiffUtil.ItemCallback<Photo>() {
+        override fun areItemsTheSame(oldItem: Photo, newItem: Photo): Boolean {
+            return oldItem.id == newItem.id
         }
 
-    }
-    override fun getItemCount(): Int {
-        return wallpapers.size
-    }
-
-    fun updateWallpapers(newWallpapers: List<Photo>) {
-        wallpapers = newWallpapers
-        notifyDataSetChanged()
+        override fun areContentsTheSame(oldItem: Photo, newItem: Photo): Boolean {
+            return oldItem == newItem
+        }
     }
 }
--- app/src/main/java/com/example/wallpaperapp/fragments/search/SearchFragment.kt
+++ app/src/main/java/com/example/wallpaperapp/fragments/search/SearchFragment.kt
@@ -8,19 +8,23 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.wallpaperapp.R
 import com.example.wallpaperapp.databinding.FragmentSearchBinding
-import com.example.wallpaperapp.fragments.wallpaper.WallpaperViewModel
 import com.example.wallpaperapp.fragments.wallpaper.onImageClick
 import com.example.wallpaperapp.manager.CategoryManager
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class SearchFragment : Fragment(), onImageClick {
 
     private lateinit var binding: FragmentSearchBinding
     private lateinit var adapter: SearchAdapter
-    private val viewModel: WallpaperViewModel by viewModels()
+
+    //    private val viewModel: WallpaperViewModel by viewModels()
+    private val viewModel: SearchViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,7 +39,6 @@ class SearchFragment : Fragment(), onImageClick {
 
         adapter = SearchAdapter(this)
         binding.apply {
-
             binding.textSearch.text = getString(R.string.start_searching)
             wallapaperRecycler.layoutManager = GridLayoutManager(context, 2)
             wallapaperRecycler.adapter = adapter
@@ -61,21 +64,17 @@ class SearchFragment : Fragment(), onImageClick {
                 Toast.makeText(requireContext(), "Please enter a search term", Toast.LENGTH_SHORT)
                     .show()
             }
-
-            Log.d("Search", "Searching for: $searchQuery")
-            viewModel.getWallpapers(searchQuery).observe(viewLifecycleOwner) { wallpapers ->
-                if (wallpapers.isNullOrEmpty()) {
-
-
-                    Toast.makeText(requireContext(), "No wallpapers found", Toast.LENGTH_SHORT)
-                        .show()
-                } else {
+            lifecycleScope.launch {
+                viewModel.getPagedWallpapers(searchQuery).collectLatest { pagingData ->
+                    Log.d("SearchFragment", "PagingData received: ${pagingData}")
                     binding.searchImage1.visibility = View.GONE
                     binding.textSearch.visibility = View.GONE
-                    adapter.updateWallpapers(wallpapers)
+                    adapter.submitData(pagingData)
                 }
             }
+
         }
+
     }
 
     override fun onPhotoClick(urlImage: String) {
