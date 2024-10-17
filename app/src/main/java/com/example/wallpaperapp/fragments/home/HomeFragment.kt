447e5c6ab38215f742515880c402154cc341836e
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2024-10-17 12:15:37 +0500

    Home Fragment

--- app/src/main/java/com/example/wallpaperapp/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/wallpaperapp/fragments/home/HomeFragment.kt
@@ -1,61 +1,56 @@
 package com.example.wallpaperapp.fragments.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.wallpaperapp.R
+import com.example.wallpaperapp.base.BaseFragment
 import com.example.wallpaperapp.databinding.FragmentHomeBinding
 import com.example.wallpaperapp.manager.CategoryManager
 import com.example.wallpaperapp.repository.ConnectivityRepository
 import com.example.wallpaperapp.viewmodel.NetworkViewModel
+import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 
-class HomeFragment : Fragment(), onCategoryItemClick {
+class HomeFragment() : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate), onCategoryItemClick {
 
-    private lateinit var binding: FragmentHomeBinding
     private lateinit var homeAdapter: HomeAdapter
     private val homeViewModel: HomeViewModel by viewModels()
     private lateinit var networkViewModel: NetworkViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentHomeBinding.inflate(layoutInflater)
-        return binding.root
-    }
+    private var previousNetworkState: Boolean? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         val connectivityRepository = ConnectivityRepository(requireContext())
 
-        networkViewModel = NetworkViewModel(connectivityRepository)
 
+        // binding use krni ha idhr
+
+        binding.apply {
+           toolbarComponent.textToolbar.text = getString(R.string.wallify)
+           homeRecycler.layoutManager = GridLayoutManager(requireContext(), 2)
+           homeRecycler.adapter = homeAdapter
+           toolbarComponent.backArrow.visibility = View.GONE
+        }
+
+        networkViewModel = NetworkViewModel(connectivityRepository)
         homeAdapter = HomeAdapter(homeViewModel.categories, this)
-        binding.toolbarComponent.textToolbar.text = getString(R.string.wallify)
-        binding.homeRecycler.layoutManager = GridLayoutManager(requireContext(), 2)
-        binding.homeRecycler.adapter = homeAdapter
-        binding.toolbarComponent.backArrow.visibility = View.GONE
 
         networkViewModel.isOnline.observe(viewLifecycleOwner) { isOnline ->
-            if (isOnline) {
-                Snackbar.make(binding.root, "Network is Online", Snackbar.LENGTH_LONG).show()
-//                showToast("Network is Back")
-            } else {
-                Snackbar.make(
-                    binding.root,
-                    "Check your Internet ! Network is Offline",
-                    Snackbar.LENGTH_LONG
-                ).show()
+            if (previousNetworkState != null && previousNetworkState != isOnline) {
+                if (isOnline) {
+                    showSnackBar(getString(R.string.network_is_online))
+                } else {
+                    showSnackBar(getString(R.string.check_your_internet_network_is_offline))
+                }
             }
+            previousNetworkState = isOnline
         }
     }
+
 
     override fun onClick(name: String) {
         findNavController().navigate(R.id.action_homeFragment_to_wallpaperFragment)
@@ -64,5 +59,15 @@ class HomeFragment : Fragment(), onCategoryItemClick {
 
     private fun showToast(message: String) {
         Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    }
+
+    private fun showSnackBar(message: String) {
+        val snack = Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG)
+        snack.setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_FADE)
+        snack.setAction("dismiss") {
+            snack.dismiss()
+            snack.setAnimationMode(R.anim.slide_up)
+        }
+        snack.show()
     }
 }
